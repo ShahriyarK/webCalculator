4241d881ac42df9493039c61a822dce0e4736796
Shahriyar Kashif <shahriyar.k@hotmail.com>
2023-04-04 12:49:07 +0500

    Added helper functions. Also, made code cleaner, added a separate class for result and clear buttons

--- calculator.js
+++ calculator.js
@@ -1,125 +1,79 @@
 const buttons = document.querySelectorAll(".btn");
-// var buttonValue = document.getElementsByClassName(".btn").value;
-console.log(buttons);
-// const buttonFirst = buttons.item(1);
-// console.log(buttonFirst);
-let sum = 0;
-let prod=1;
+const specialButtons = document.querySelectorAll(".btn-sp");
 let buttonString = "";
 let numArray=[];
-let quot = numArray;
+let result = 0;
+
 for(let button of buttons) {
     button.addEventListener('click',() => {
         let buttonValue = button.innerHTML;
-        buttonString = buttonString + buttonValue;
-        console.log(buttonString);
+        buttonString += buttonValue;
+        // console.log(buttonString);
+        document.getElementById('display').textContent = buttonString;
 
-        if (buttonValue === "C") {
-            console.log("if start");
+        if (buttonString.includes("+") && buttonValue !== '+') {
+            numArray = buttonString.split('+');
+            result = sumOf(numArray);
+        }
+        else if (buttonString.includes("x") && buttonValue !== 'x') {
+            numArray = buttonString.split('x');
+            result = productOf(numArray);
+        }
+        else if (buttonString.includes("÷") && buttonValue !== '÷') {
+            numArray = buttonString.split('÷');
+            result = divisionOf(numArray);
+        }
+        else if (buttonString.includes("-") && buttonValue !== '-') {
+            numArray = buttonString.split('-');
+            result = differenceOf(numArray);
+            console.log(result);
+        }
+    })
+}
+
+for (let button of specialButtons) {
+    button.addEventListener('click', () =>{
+        let buttonValue = button.innerHTML;
+        if (buttonValue === '=') {
+            document.getElementById('display').textContent = result.toString();
+        }
+        if (buttonValue === 'C') {
             document.getElementById('display').textContent = "0";
             buttonString = "";
         }
-
-        else {
-        console.log(buttonValue);
-        document.getElementById('display').textContent = buttonString;}
-
-        if (buttonString.includes("+") && !buttonString.includes("=")) {
-
-            sum = 0;
-            console.log(numArray, numArray.length);
-            numArray = buttonString.split('+');
-            for (let i = 0; i < numArray.length && buttonValue !== '+'; i++) {
-                console.log("if 1 start");
-                sum += Number(numArray[i]);
-                console.log(sum);
-            }
-        }
-        else if (buttonString.includes("=")&& buttonString.includes("+") ) {
-            console.log("result if")
-            console.log(sum);
-            document.getElementById('display').textContent = sum.toString();
-
-        }
-
-        if (buttonString.includes("x") && !buttonString.includes("=")) {
-
-            prod = 1;
-            console.log(numArray, numArray.length);
-            numArray = buttonString.split('x');
-            for (let i = 0; i < numArray.length && buttonValue !== 'x'; i++) {
-                console.log("if 1 start");
-                prod *= Number(numArray[i]);
-                // console.log(sum);
-            }
-        }
-        else if (buttonString.includes("=") && buttonString.includes("x") ) {
-            console.log("result if")
-            console.log(prod);
-            document.getElementById('display').textContent = prod.toString();
-        }
-
-        if (buttonString.includes("x") && !buttonString.includes("=")) {
-
-            prod = 1;
-            console.log(numArray, numArray.length);
-            numArray = buttonString.split('x');
-            for (let i = 0; i < numArray.length && buttonValue !== 'x'; i++) {
-                console.log("if 1 start");
-                prod *= Number(numArray[i]);
-                // console.log(sum);
-            }
-        }
-        else if (buttonString.includes("=") && buttonString.includes("x") ) {
-            console.log("result if")
-            console.log(prod);
-            document.getElementById('display').textContent = prod.toString();
-        }
-
-        if (buttonString.includes("÷") && !buttonString.includes("=")) {
-
-
-            console.log(numArray, numArray.length);
-            numArray = buttonString.split('÷');
-            quot = Number(numArray[0]);
-            for (let i = 1; i < numArray.length && buttonValue !== '÷'; i++) {
-                console.log("if 1 start");
-                quot = quot / Number(numArray[i]);
-                console.log(quot);
-            }
-        }
-        else if (buttonString.includes("=") && buttonString.includes("÷") ) {
-            console.log("result if")
-            // console.log(prod);
-            document.getElementById('display').textContent = quot.toString();
-        }
-
-        if (buttonString.includes("-") && !buttonString.includes("=")) {
-
-            minus = 0;
-            console.log(numArray, numArray.length);
-            numArray = buttonString.split('-');
-            for (let i = 0; i < numArray.length && buttonValue !== '-'; i++) {
-                console.log("if 1 start");
-                minus = -minus-Number(numArray[i]) ;
-                // console.log(sum);
-            }
-        }
-        else if (buttonString.includes("=") && buttonString.includes("-") ) {
-            console.log("result if")
-            // console.log(prod);
-            document.getElementById('display').textContent = minus.toString();
-        }
     })
-    // {
-    //     let buttonValue = button.value;
-    // }
-    // console.log(buttons[i]);
-
 }
-// function removeElm() {
-//     let buttonValue = button.value;
-//     document.getElementById('display').textContent = buttonValue;
-//     // document.getElementById('display').textContent = buttons[0].innerHTML;
-// //     console.log(buttons.indexOf(7));
-// }
+
+
+
+function sumOf(array) {
+    let sum = 0;
+    for (i = 0; i < array.length; i++) {
+        sum += Number(array[i]);
+    }
+    return sum;
+}
+
+function productOf(array) {
+    let prod = 1;
+    for (i = 0; i < array.length; i++) {
+        prod *= Number(array[i]);
+    }
+    return prod;
+}
+
+function differenceOf(array) {
+    let diff = Number(array[0]);
+    for (i = 1; i < array.length; i++) {
+        diff = diff - Number(array[i]);
+    }
+    return diff;
+}
+
+function divisionOf(array) {
+    let div = Number(array[0]);
+    for (i = 1; i < array.length; i++) {
+        div = div / Number(array[i]);
+    }
+    return div;
+}
